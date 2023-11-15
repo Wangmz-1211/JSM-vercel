ae6982eb877a4b52453dcf3feb7858a76d5328bc
wangmz <w2639764449@hotmail.com>
2023-11-15 14:46:32 +0800

    create a preview envionment

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
 	return (
 		<footer className='w-full h-16 mt-8  bg-background'>
 			<div className='w-11/12 h-16 border-t border-secondary py-4 mx-auto text-center text-primary'>
-				<span>✨ code by wangmz</span>
+				<span>✨ preview environment</span>
 			</div>
 		</footer>
 	)
