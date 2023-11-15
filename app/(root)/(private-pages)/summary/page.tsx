45dd950192c86d75077b029526608d44a76b426a
wangmz <w2639764449@hotmail.com>
2023-11-16 00:04:01 +0800

    change engine

--- app/(root)/(private-pages)/summary/page.tsx
+++ app/(root)/(private-pages)/summary/page.tsx
@@ -35,8 +35,6 @@ const Summary = async () => {
         <div className="w-max h-full flex-1 pt-4 mx-auto">
             <SummaryCharts records={sortedRecords}/>
             <SummaryAnalysis records={sortedRecords}/>
-
-            <span className="text-center mx-auto w-full inline-block mt-4 text-jred">✨ Coming feature: analysis the trend by AI</span>
         </div>
     )
 }
