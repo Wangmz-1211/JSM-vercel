9c6cdcdf4880aeb7f8b318c6e842ffabb66dbf06
wangmz <w2639764449@hotmail.com>
2023-11-17 21:07:57 +0800

    👽️ edge block hkg region

--- app/(root)/api/chat/route.ts
+++ app/(root)/api/chat/route.ts
@@ -1,7 +1,30 @@
 import {OpenAI} from "openai";
 import {OpenAIStream, StreamingTextResponse} from 'ai'
 
-export const runtime = "edge"
+export const config = {
+    runtime: 'edge',
+    // https://vercel.com/docs/edge-network/regions#region-list
+    regions: [
+        'arn1',
+        'bom1',
+        'cdg1',
+        'cle1',
+        'cpt1',
+        'dub1',
+        'fra1',
+        'gru1',
+        // 'hkg1', ip blocked by openai
+        'hnd1',
+        'iad1',
+        'icn1',
+        'kix1',
+        'lhr1',
+        'pdx1',
+        'sfo1',
+        'sin1',
+        'syd1',
+    ],
+};
 
 const openai = new OpenAI({
     apiKey: process.env.OPENAI_KEY,
