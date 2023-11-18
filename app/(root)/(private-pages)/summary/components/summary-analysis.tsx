2dfe38523b653cb15a6a591abd15e436bd805171
wangmz <w2639764449@hotmail.com>
2023-11-19 00:18:32 +0800

    🐛 stop ai generation after switching page

--- app/(root)/(private-pages)/summary/components/summary-analysis.tsx
+++ app/(root)/(private-pages)/summary/components/summary-analysis.tsx
@@ -1,58 +1,47 @@
-'use client'
+"use client";
 import ChatCardMessage from "@/app/(root)/(private-pages)/chat/components/chat-card-message";
-import {useEffect} from "react";
-import {useChat} from "ai/react";
+import { useEffect } from "react";
+import { useChat } from "ai/react";
 
 const cleanRecord = (record: Record<string, any>) => {
-	delete record.vocabulary
-	delete record.grammar
-	delete record.listening
-	delete record.id
-	delete record.reading
-	delete record.v
-	delete record.user_email
-	return record
-}
-export const SummaryAnalysis = (
-		{
-			records
-		}: {
-			records: Array<any>
-		}
-	) => {
-		const {messages, append} = useChat({
-			api: '/api/chat/analysis',
+  delete record.vocabulary;
+  delete record.grammar;
+  delete record.listening;
+  delete record.id;
+  delete record.reading;
+  delete record.v;
+  delete record.user_email;
+  return record;
+};
+export const SummaryAnalysis = ({ records }: { records: Array<any> }) => {
+  const { messages, append, stop } = useChat({
+    api: "/api/chat/analysis",
+  });
+  const cleanedRecords = records.map((record) => cleanRecord(record));
 
-		})
-		const cleanedRecords = records.map(record => cleanRecord(record))
+  useEffect(() => {
+    console.log("generating analysis...");
+    append({
+      role: "user",
+      content: JSON.stringify(cleanedRecords),
+    });
+    return () => {
+      stop();
+    };
+  }, []);
 
-		useEffect(() => {
-				console.log('generating analysis...')
-				append({
-						role: 'user',
-						content: JSON.stringify(cleanedRecords)
-					}
-				)
-			}, []
-		)
-		;
+  return (
+    <div className="w-screen">
+      <section className="overflow-y-scroll mt-4">
+        {messages.map((m) => {
+          if (m.role === "user") return null;
+          return (
+            <ChatCardMessage key={m.id} role={m.role} content={m.content} />
+          );
+        })}
+      </section>
+    </div>
+  );
+};
 
-		return (
-
-			<div className="w-screen">
-				<section className="overflow-y-scroll mt-4">
-					{messages.map(m => {
-							if (m.role === 'user') return null
-							return (
-								<ChatCardMessage key={m.id} role={m.role} content={m.content}/>
-							)
-						}
-					)}
-				</section>
-			</div>
-		)
-			;
-	}
-;
-
-export default SummaryAnalysis
+export default SummaryAnalysis;
